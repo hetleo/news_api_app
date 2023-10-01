d2051946cef7d16f6412e7f7f436694cf07935b8
hetleo <hetleo@gmail.com>
2023-10-01 17:06:14 +0200

    security changes

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import requests
+from functions import send_email
 
 api_key = "335a80ec984e4986b3fccdd0c82e21cf"
-url = 'https://newsapi.org/v2/everything?q=("public library" AND vancouver)&from=2023-08-17&sortBy=publishedAt&apiKey=335a80ec984e4986b3fccdd0c82e21cf'
+url = f'https://newsapi.org/v2/everything?q=iota and wallet&from=2023-09-10&apiKey={api_key}'
 
 # Make request
 request = requests.get(url)
 
-# Make a dict with data
-content = request.json()
+# Get a dictionary with data
+content = request.json()    
 
-# Access the article title and description
+# Access the article titles and description
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
-    print("- - - - -")
+    title = article["title"]
+    desc = article["description"]
+    article_url = article["url"]
+    body = body + f"{title.upper()} \n{desc} \n{article_url} \n\n"
+
+print(body)
+# body = body.encode("utf-8")
+# send_email(message=body)
